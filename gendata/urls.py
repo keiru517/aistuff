c1c649186d250afd15e5d177cc1f8a6823b8e2c2
keiru517 <adrianbotteon@gmail.com>
2023-04-10 12:30:53 +0300

    updated url

--- gendata/urls.py
+++ gendata/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='gendata'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('generate_rsa/', views.generate_rsa, name='generate_rsa')
 ]
--- gendata/views.py
+++ gendata/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def generate_rsa(request):
+    return render(request, 'prompt.html')
+
+def generate_sitelink(request):
+    pass
