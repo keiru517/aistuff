fe57cebd4fd7386504c3b15e80f7b121a2a9bd4c
keiru517 <adrianbotteon@gmail.com>
2023-04-13 03:19:46 +0300

    updated template

--- gendata/views.py
+++ gendata/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+import openai
 
+openai.api_key = "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"
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def generate_rsa(request):
+def prompt(request):
     return render(request, 'prompt.html')
+
+def generate_rsa(request):
+    prompt = request.POST.get('rsa_prompt')
+    
+    messages = []
+    messages.append({"role": "system", "content": "Your name is Karabo. You are a helpful assistant."})
+
+    question = {}
+    question['role'] = 'user'
+    question['content'] = prompt
+    messages.append(question)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
+    
+    response = response['choices'][0]['message']['content']
+    print("Resonse====================================================: ", response)
+    
+    return redirect('gendata:index')
 
 def generate_sitelink(request):
     pass
